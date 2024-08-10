64679798404c73aa6d51117af25c4d6eb7c7e807
qa-ruslan <GalyautdinovRV@gmail.com>
2024-08-10 19:09:45 +0500

    Добавил кпонку скачать резюме

--- js.js
+++ js.js
@@ -3,5 +3,8 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('В разработке, спросите в телеграме :)')
+	const link = document.createElement('a');
+	link.href = './cv.pdf'; // Укажите путь к вашему файлу
+	link.download = 'cv.pdf'; // Имя файла для скачивания
+	link.click();
 })
